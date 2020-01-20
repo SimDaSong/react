36bdcc94cc348555788024e457e4bd0994d477ab
SimDaSong <song98987@korea.ac.kr>
2020-01-20 13:05:18 +0900

    using jsx in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+class Subject extends Component {
+  // Component는 React의 클래스
+  render() {
+    // class 내에서 함수는 function이라는 키워드 안적어도됨
+    return (
+      // 하나의 최상위 태그만 써야 함
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  // TOC: Table of contents. 목차
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText MarkUp Language.
+      </article>
+    );
+  }
+}
 
 class App extends Component {
+  // Component는 React의 클래스
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
