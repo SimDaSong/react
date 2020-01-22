183df8820182fa897db35269129ab1179de4d1b4
SimDaSong <song98987@korea.ac.kr>
2020-01-22 10:41:52 +0900

    state 활용

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,7 +11,12 @@ class App extends Component {
     super(props);
     this.state = {
       // state 값 초기화
-      subject: { title: "WEB", sub: "World Wide Web" } // state 값
+      subject: { title: "WEB", sub: "World Wide Web" }, // subject라는 property에 값 하나.
+      contents: [ // TOC의 부모가 가지고 있는 state를 이용해서 TOC의 내부 내용이 바뀌도록 할 것
+        {id:1, title:'HTML', desc:'HTML is HyperText ...'},
+        {id:1, title:'CSS', desc:'CSS is for design'},
+        {id:1, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
     };
   }
 
@@ -22,7 +27,7 @@ class App extends Component {
           title={this.state.subject.title} // 상위 컴포넌트인 App의 상태를 하위 컴포넌트인 Subject로 전달하고 싶을 땐 상위 컴포넌트의 state 값을 하위 컴포넌트의 props의 값으로 전달하는 것은 얼마든지 가능하다.
           sub={this.state.subject.sub}
         ></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC> 
         <Content
           title="HTML"
           desc="HTML is HyperText MarkUp Language."
--- react-app/src/components/TOC.js
+++ react-app/src/components/TOC.js
@@ -3,6 +3,11 @@ import React, { Component } from "react"; // 필수로 넣기
 class TOC extends Component {
     // TOC: Table of contents. 목차
     render() {
+      var lists = [];
+      var data = this.props.data;
+      for (var i = 0; i < data.length; i++) {
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+      }
       return (
         <nav>
           <ul>
