32eb7d66a29a953d5a2ae7b168ea61efa1d65ae5
SimDaSong <song98987@korea.ac.kr>
2020-01-26 14:43:07 +0900

    컴포넌트 이벤트

--- react-app/src/components/Subject.js
+++ react-app/src/components/Subject.js
@@ -7,7 +7,10 @@ class Subject extends Component {
         return (
             // 하나의 최상위 태그만 써야 함
             <header>
-                <h1><a href="/">{this.props.title}</a></h1> 
+                <h1><a href="/" onClick={function(e){
+                    e.preventDefault(); // 링크를 클릭 했을 때 페이지가 바뀌는 것을 막음
+                    this.props.onChangePage();
+                }.bind(this)}>{this.props.title}</a></h1> 
                 {this.props.sub}
             </header>
         );
